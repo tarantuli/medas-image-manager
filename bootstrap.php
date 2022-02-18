3f6b3d77b38fb6501a5b42581a948b6e23e8cfc5
Patrick Morpey <patrick@morphp.nl>
2022-02-18 14:42:31 +0100

    Removed unnecessary copied code

--- bootstrap.php
+++ bootstrap.php
@@ -2,11 +2,8 @@
 
 declare(strict_types=1);
 
-use Medas\JsonLdToAngularInterfaces\JsonLdToAngularInterfacesPackage;
 use Medas\Placeholder\PlaceholderPackage;
 use Medas\ServiceManager\ServiceManager;
-use Symfony\Component\Cache\Adapter\ApcuAdapter;
-use Symfony\Contracts\Cache\CacheInterface;
 
 chdir(__DIR__);
 

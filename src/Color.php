51eff9ed047a159f58e76f741a9f79e5fcc7f3df
Patrick Morpey <patrick@morphp.nl>
2024-01-12 12:18:30 +0100

    Such nonsense

--- src/Color.php
+++ src/Color.php
@@ -6,8 +6,6 @@ namespace Medas\ImageManager;
 
 class Color
 {
-    public string $hexString;
-
     public function __construct(
         public float $red = 0.0,
         public float $green = 0.0,
@@ -15,9 +13,5 @@ class Color
         public float $opacity = 1.0,
     )
     {
-        $this->hexString = str_pad(dechex((int) round(255 * $this->red)), 2, '0', STR_PAD_LEFT)
-            . str_pad(dechex((int) round(255 * $this->green)), 2, '0', STR_PAD_LEFT)
-            . str_pad(dechex((int) round(255 * $this->blue)), 2, '0', STR_PAD_LEFT)
-            . str_pad(dechex((int) round(255 * $this->opacity)), 2, '0', STR_PAD_LEFT);
     }
 }

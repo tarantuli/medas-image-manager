19ab745dcc26300d2426cf74b56ec367ec9ff9e1
Patrick Morpey <patrick@morphp.nl>
2024-01-12 11:34:08 +0100

    Added hexString

--- src/Color.php
+++ src/Color.php
@@ -6,6 +6,8 @@ namespace Medas\ImageManager;
 
 class Color
 {
+    public string $hexString;
+
     public function __construct(
         public float $red = 0.0,
         public float $green = 0.0,
@@ -13,5 +15,9 @@ class Color
         public float $opacity = 1.0,
     )
     {
+        $this->hexString = str_pad(dechex(255 * $this->red), 2, '0', STR_PAD_LEFT)
+            . str_pad(dechex(255 * $this->green), 2, '0', STR_PAD_LEFT)
+            . str_pad(dechex(255 * $this->blue), 2, '0', STR_PAD_LEFT)
+            . str_pad(dechex(255 * $this->opacity), 2, '0', STR_PAD_LEFT);
     }
 }
